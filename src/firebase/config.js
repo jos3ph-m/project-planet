93e5711b04958764c2d877568110370670950afc
Joseph M <jmedina1985@hotmail.com>
2022-04-25 15:07:38 -0700

    Firebase setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,7 +5,7 @@ import 'firebase/auth';
 const firebaseConfig = {
   apiKey: process.env.REACT_FIREBASE_APIKEY,
   authDomain: 'projectplanet-44124.firebaseapp.com',
-  projectId: 'projectplanet-44124',
+  projectId: 'projectplanet',
   storageBucket: 'projectplanet-44124.appspot.com',
   messagingSenderId: '1064281956748',
   appId: process.env.REACT_FIREBASE_APPID,

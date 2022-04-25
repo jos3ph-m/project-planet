d50214ef1cad0b1217c9c6bf1c594529fcbe2e1f
Joseph M <jmedina1985@hotmail.com>
2022-04-25 15:08:09 -0700

    Firebase setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,7 +18,6 @@ firebase.initializeApp(firebaseConfig);
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
 
-// timestamp
 const timestamp = firebase.firestore.Timestamp;
 
 export { projectFirestore, projectAuth, timestamp };

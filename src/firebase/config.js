3e603f2186adeb6459dd5548b216492d3a6a5281
Joseph M <jmedina1985@hotmail.com>
2022-04-25 14:59:35 -0700

    Getting started

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_FIREBASE_APIKEY,
+  authDomain: 'projectplanet-44124.firebaseapp.com',
+  projectId: 'projectplanet-44124',
+  storageBucket: 'projectplanet-44124.appspot.com',
+  messagingSenderId: '1064281956748',
+  appId: process.env.REACT_FIREBASE_APPID,
+};

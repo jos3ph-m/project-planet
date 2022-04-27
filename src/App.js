7dbfe91277c37086bc4633d2af4c1e1dd00db5f5
Joseph M <jmedina1985@hotmail.com>
2022-04-27 16:02:01 -0700

    Re-using Firebase Hooks & Auth Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
+
+/* pages
+
+ - dashboard (homepage)
+ - login 
+ - signup
+ - create
+ - project (project details)
+
+*/

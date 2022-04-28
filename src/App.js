bdeedecbdbce33c5705e41e2894adcaa65fe5a60
Joseph M <jmedina1985@hotmail.com>
2022-04-28 13:49:47 -0700

    Router and pages setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // styles
 import './App.css';
 
+// pages and components
+import Dashboard from './pages/dashboard/Dashboard';
+
 function App() {
   return <div className="App"></div>;
 }

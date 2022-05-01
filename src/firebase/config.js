77cde6034c5f0c511e977b95b6bf50dfabaca086
Joseph M <jmedina1985@hotmail.com>
2022-04-30 18:57:08 -0700

    Router and pages setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_FIREBASE_APIKEY,
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: 'projectplanet-44124.firebaseapp.com',
   projectId: 'projectplanet-44124',
   storageBucket: 'projectplanet-44124.appspot.com',
   messagingSenderId: '1064281956748',
-  appId: process.env.REACT_FIREBASE_APPID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 // init firebase

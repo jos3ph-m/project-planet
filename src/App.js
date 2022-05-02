d8349250c494d2741c72fe158c1714074e2442e9
Joseph M <jmedina1985@hotmail.com>
2022-05-01 18:28:23 -0700

    Router and pages setup

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ function App() {
             <Route path="/projects/:id">
               <Project />
             </Route>
-            <Route path="/login"></Route>
+            <Route path="/login">
+              <Login />
+            </Route>
             <Route path="/signup">
               <Signup />
             </Route>

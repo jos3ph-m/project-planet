d96fe9e5b9fcc515b74a8e29304f97eeda385a5c
Joseph M <jmedina1985@hotmail.com>
2022-05-03 14:16:31 -0700

    Making the Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
-// styles
+// styles & images
 import './Sidebar.css';
+import DashboardIcon from '../assets/dashboard_icon.svg';
 
 export default function Sidebar() {
   return <div>Sidebar</div>;

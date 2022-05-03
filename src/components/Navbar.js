a4fb8800b03524ef57510438dcfaf5c3fe9013c8
Joseph M <jmedina1985@hotmail.com>
2022-05-03 10:43:18 -0700

    Making the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,6 +18,9 @@ export default function Navbar() {
         <li>
           <Link to="signup">Sign Up</Link>
         </li>
+        <li>
+          <button className="btn">Logout</button>
+        </li>
       </ul>
     </div>
   );

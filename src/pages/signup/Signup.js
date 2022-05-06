2c8102deb7ef0783058fd2d7843647cfdee3a0d6
Joseph M <jmedina1985@hotmail.com>
2022-05-06 13:19:30 -0700

    Creating a signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -9,5 +9,5 @@ export default function Signup() {
   const [displayName, setDisplayName] = useState('');
   const [thumbnail, setThumbnail] = useState(null);
 
-  return <div></div>;
+  return <div>Signup page</div>;
 }

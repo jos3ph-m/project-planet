57a0a0479184bd701f6502623942cde814dacb07
Joseph M <jmedina1985@hotmail.com>
2022-05-06 17:11:02 -0700

    Creating a signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -4,5 +4,10 @@ import { useState } from 'react';
 import './Signup.css';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
   return <div>{displayName}</div>;
 }

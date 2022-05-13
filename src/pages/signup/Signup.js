c8680d321fcb0bb0c301e10eaa5d923f4e57b7df
Joseph M <jmedina1985@hotmail.com>
2022-05-13 12:14:48 -0700

    Handling file input

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -52,7 +52,6 @@ export default function Signup() {
           required
           type="password"
           onChange={(e) => setPassword(e.target.value)}
-          value={password}
         />
       </label>
       <label>

2ace10ef2a7f20ebe4a4ee2956cfb0cc7e27d639
Joseph M <jmedina1985@hotmail.com>
2022-05-12 17:15:22 -0700

    Handling file input

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -67,7 +67,7 @@ export default function Signup() {
       <label>
         <span>profile thumbnail:</span>
         <input required type="file" onChange={handleFileChange} />
-        {thumbnailError && <div>{thumbnailError}</div>}
+        {thumbnailError && <div>{}</div>}
       </label>
       <button className="btn">Sign up</button>
     </form>

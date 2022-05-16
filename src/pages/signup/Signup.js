5a047c70f85714e92613f3b887b6d9c4457cbb3c
Joseph M <jmedina1985@hotmail.com>
2022-05-16 14:36:48 -0700

    Handle file input

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -12,7 +12,7 @@ export default function Signup() {
 
   const handleSubmit = (e) {
     e.preventDefault();
-    console.log(emai, password);
+    console.log(emai, password, displayName);
   }
 
   const handleFileChange = (e) => {

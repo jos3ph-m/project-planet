a4e8f54cee5db8203284631d0a8f526418290768
Joseph M <jmedina1985@hotmail.com>
2022-05-17 16:39:07 -0700

    Firebase storage setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,8 +18,9 @@ firebase.initializeApp(firebaseConfig);
 // init services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, projectStorage, timestamp };
+export { projectFirestore, projectAuth, timestamp };

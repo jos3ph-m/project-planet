4eb618e23151f087313f2719f482c90b6647ba5e
Joseph M <jmedina1985@hotmail.com>
2022-05-17 16:38:12 -0700

    Firebase storage setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,

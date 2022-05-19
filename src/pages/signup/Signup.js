4db10a305f347fc347940a0e36972af5e9304db9
Joseph M <jmedina1985@hotmail.com>
2022-05-19 15:44:53 -0700

    Signing a user up

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -77,6 +77,7 @@ export default function Signup() {
         {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
       <button className="btn">Sign up</button>
+      {error && <div></div>}
     </form>
   );
 }

1191701098d23605e4ba4841557998bf0eb065f5
Joseph M <jmedina1985@hotmail.com>
2022-05-23 10:41:43 -0700

    Making a login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -34,13 +34,13 @@ export default function Login() {
         />
       </label>
 
-      {!isPending && <button className="btn">Login</button>}
+      {/*!isPending && <button className="btn">Login</button>}
       {isPending && (
         <button className="btn" disabled>
           Loading
         </button>
       )}
-      {error && <div className="error">{error}</div>}
+      {error && <div className="error">{error}</div>*/}
     </form>
   );
 }

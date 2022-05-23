b3b485c51b28ac00fc089694f987e6068b1fbd65
Joseph M <jmedina1985@hotmail.com>
2022-05-23 10:39:08 -0700

    Making a login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,5 +2,35 @@
 import './Login.css';
 
 export default function Login() {
-  return <div></div>;
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Lgoin</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      {!isPending && <button className="btn">Sign up</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
 }

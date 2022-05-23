b46c0af51d520f47c4dcd923e5f7c2c04eb08409
Joseph M <jmedina1985@hotmail.com>
2022-05-23 10:39:26 -0700

    Making a login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -24,7 +24,7 @@ export default function Login() {
         />
       </label>
 
-      {!isPending && <button className="btn">Sign up</button>}
+      {!isPending && <button className="btn">Login</button>}
       {isPending && (
         <button className="btn" disabled>
           Loading

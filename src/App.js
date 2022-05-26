29b5d00510f66d513c213a318f36ffbedc1687e5
Joseph M <jmedina1985@hotmail.com>
2022-05-26 11:08:51 -0700

    Redirects and Route Guards

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             <Navbar />
             <Switch>
               <Route exact path="/">
-                <Dashboard />
+                {user && <Dashboard />}
               </Route>
               <Route path="/create">
                 <Create />

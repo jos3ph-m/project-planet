a9a870ffeac79e5d55c16f50c059333e954bf2cc
Joseph M <jmedina1985@hotmail.com>
2022-05-27 13:58:07 -0700

    conditional navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
+
 // styles & images
 import './Navbar.css';
 import Planet from '../assets/planet.svg';
 
 export default function Navbar() {
   const { logout, isPending } = useLogout();
+  const { user, authIsReady } = useAuthContext();
 
   return (
     <div className="navbar">

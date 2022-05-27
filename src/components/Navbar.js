c9a4a7034c2ad00ae0881f6067284531beca3e0f
Joseph M <jmedina1985@hotmail.com>
2022-05-27 13:59:04 -0700

    conditional navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,14 +17,14 @@ export default function Navbar() {
           <img src={Planet} alt="planet logo" />
           <span>Project Planet</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        {!user && (
-          <li>
-            <Link to="signup">Sign Up</Link>
-          </li>
-        )}
+        {!user &&
+          (<li>
+            <Link to="/login">Login</Link>
+          </li>)(
+            <li>
+              <Link to="signup">Sign Up</Link>
+            </li>
+          )}
         <li>
           {!isPending && (
             <button className="btn" onClick={logout}>

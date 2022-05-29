211e87c655bce4df47367b145797c40e3dd75150
Joseph M <jmedina1985@hotmail.com>
2022-05-29 16:11:39 -0700

    User avatar component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -4,7 +4,7 @@ import './Avatar.css';
 export default function Avatar({ src }) {
   return (
     <div className="avatar">
-      <img src={src} alt="avatar image" />
+      <img src={src} alt="" />
     </div>
   );
 }

f2616277d4766895f13384bd30a0f5c930f10e56
Joseph M <jmedina1985@hotmail.com>
2022-06-01 14:34:52 -0700

    Fetching users

--- src/components/OnlineUsers.js
+++ src/components/OnlineUsers.js
@@ -4,6 +4,7 @@ import { useCollection } from '../hooks/useCollection';
 import './OnlineUsers.css';
 
 export default function OnlineUsers() {
+  const { error, documents } = useCollection('users');
   return (
     <div className="user-list">
       <h2>All Users</h2>

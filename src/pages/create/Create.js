fc7a146e97decc83b519b3d622a84b539bf819a3
Joseph M <jmedina1985@hotmail.com>
2022-06-03 12:46:09 -0700

    Making the Create Project form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -5,6 +5,7 @@ export default function Create() {
   return (
     <div className="create-form">
       <h2 className="page-title">Create a new project</h2>
+      <form></form>
     </div>
   );
 }

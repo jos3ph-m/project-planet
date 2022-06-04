343022eb3be828a16bece146d7137d5ecedb2120
Joseph M <jmedina1985@hotmail.com>
2022-06-04 16:25:03 -0700

    Making the Create Project form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -6,7 +6,7 @@ export default function Create() {
   const [name, setName] = useState('');
 
   return (
-    <div>
+    <div className="create-form">
       <h2 className="page-title">Create a new project</h2>
       <form></form>
     </div>

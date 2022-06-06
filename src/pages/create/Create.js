6362f55e71413680f5d01d87e6e120191255a334
Joseph M <jmedina1985@hotmail.com>
2022-06-06 12:26:19 -0700

    Making the Create Project form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -43,6 +43,7 @@ export default function Create() {
             value={dueDate}
           ></textarea>
         </label>
+        <button className="btn">Add Project</button>
       </form>
     </div>
   );

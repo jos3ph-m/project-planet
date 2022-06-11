4762db8f2d0b7d14cbd96ceeabf8b2732e4a6f59
Joseph M <jmedina1985@hotmail.com>
2022-06-11 16:53:19 -0700

    Using React-Select

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -69,7 +69,7 @@ export default function Create() {
           ></input>
         </label>
         <label>
-          <span>Project category:</span>
+          <span></span>
           <Select
             onChange={(option) => setCategory(option)}
             options={categories}

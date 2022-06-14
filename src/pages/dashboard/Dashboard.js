82a16ced2115fc1d0daf4d92127f395e7492da62
Joseph M <jmedina1985@hotmail.com>
2022-06-14 13:14:32 -0700

    Creating a Project Object

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -9,6 +9,7 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && documents.map((doc) => <p key={doc.id}>{doc.name}</p>)}
     </div>
   );
 }

b1dc68942c057e21847d5eba82163709da89c6a0
Joseph M <jmedina1985@hotmail.com>
2022-06-14 13:12:47 -0700

    Creating a Project Object

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,5 +4,6 @@ import { useCollection } from '../../hooks/useCollection';
 import './Dashboard.css';
 
 export default function Dashboard() {
+  const { documents, error } = useCollection('projects');
   return <div>Dashboard</div>;
 }

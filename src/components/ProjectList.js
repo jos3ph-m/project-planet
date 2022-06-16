cdcedb0623e6f29704259b417ced7bcc4c2d839c
Joseph M <jmedina1985@hotmail.com>
2022-06-16 11:12:48 -0700

    Making a ProjectList - Grid

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -8,7 +8,9 @@ export default function ProjectList({ projects }) {
     <div>
       {projects.length === 0 && <p>No projects yet.</p>}
       {projects.map((project) => (
-        <Link to={`/projects${project.id}`} key={project.id}></Link>
+        <Link to={`/projects${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+        </Link>
       ))}
     </div>
   );

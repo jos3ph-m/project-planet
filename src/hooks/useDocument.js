44864e297976d96bc486533c2ee0aa0d8bbd0ddf
Joseph M <jmedina1985@hotmail.com>
2022-06-19 11:03:42 -0700

    Making a useDocument hook

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
 import { projectFirestore } from '../firebase/config';
 
-export const useDocument = (collection, id) => {};
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null);
+};

e2b19b53878868a3aa615e35bfc1665fe3a9175e
Joseph M <jmedina1985@hotmail.com>
2022-06-23 11:53:42 -0700

    Project details

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -2,7 +2,7 @@ export default function ProjectSummary({ project }) {
   return (
     <div>
       <div className="project-summary">
-        <h2></h2>
+        <h2 className="page-title"></h2>
       </div>
     </div>
   );

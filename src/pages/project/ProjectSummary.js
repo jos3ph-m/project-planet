f77cb5ecaad8514d4d44feb303a9ad779cf15a83
Joseph M <jmedina1985@hotmail.com>
2022-06-24 11:53:32 -0700

    Project details

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -3,7 +3,7 @@ export default function ProjectSummary({ project }) {
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
-        <p className="due-date">Project due by</p>
+        <p className="due-date">Project due by {}</p>
       </div>
     </div>
   );

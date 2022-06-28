eed7bc37a85a93dc776dbcce149cc0873576fd89
Joseph M <jmedina1985@hotmail.com>
2022-06-28 11:22:57 -0700

    Making a comments form

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { timestamp } from '../../firebase/config';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export default function ProjectComments() {
   return <div>ProjectComments</div>;

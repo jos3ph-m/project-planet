147d4e3f83aaa4c1b5596c40a87f7e12869a2266
Joseph M <jmedina1985@hotmail.com>
2022-06-29 11:52:29 -0700

    Making a comments form

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -3,13 +3,14 @@ import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 
 export default function ProjectComments() {
+  const [newComment, setNewComment] = useState('')
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
       <form className="add-comment">
         <label>
           <span>Add new comment:</span>
-          <textarea required></textarea>
+          <textarea required onChange={}></textarea>
         </label>
       </form>
     </div>

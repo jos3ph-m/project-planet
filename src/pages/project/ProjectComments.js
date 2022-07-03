9d94a4ef98eecb567caa09132144254d6700a8aa
Joseph M <jmedina1985@hotmail.com>
2022-07-03 10:24:57 -0700

    listing comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -38,7 +38,10 @@ export default function ProjectComments({ project }) {
         {project.comments.length > 0 &&
           project.comments.map((comment) => (
             <li key={comment.id}>
-              <div className="comment-author"></div>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
             </li>
           ))}
       </ul>

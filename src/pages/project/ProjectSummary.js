a941d0448340445a261e0862492f3641675f0cd8
Joseph M <jmedina1985@hotmail.com>
2022-07-05 10:33:55 -0700

    Completing and deleting projects

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -26,11 +26,10 @@ export default function ProjectSummary({ project }) {
             </div>
           ))}
         </div>
-        {user.uid === project.createdBy.id && (
-          <button className="btn" onClick={handleClick}>
-            Mark as complete
-          </button>
-        )}
+        {user.uid === project.createdBy.id && ()}
+        <button className="btn" onClick={handleClick}>
+          Mark as complete
+        </button>
       </div>
     </div>
   );

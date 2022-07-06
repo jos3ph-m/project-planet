7ef4ccf2952966926712f6a00955a7615b33bf7c
Joseph M <jmedina1985@hotmail.com>
2022-07-06 12:07:29 -0700

    Completing and deleting projects

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router-dom';
 export default function ProjectSummary({ project }) {
   const { deleteDocument } = useFirestore('projects');
   const { user } = useAuthContext();
+  const history = useHistory();
 
   const handleClick = (e) => {
     deleteDocument(project.id);

25f41a7e0ced03cdc087abca5716d9323c67a53f
Joseph M <jmedina1985@hotmail.com>
2022-07-06 12:00:04 -0700

    Completing and deleting projects

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -14,6 +14,7 @@ export default function ProjectSummary({ project }) {
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p></p>
         <p className="due-date">
           Project due by {project.dueDate.toDate().toDateString()}
         </p>

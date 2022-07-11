1fbb1f329dbb3535a157b35343a54033565b5e8d
Joseph M <jmedina1985@hotmail.com>
2022-07-11 12:38:45 -0700

    Making a filter

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -8,6 +8,11 @@ import './Dashboard.css';
 export default function Dashboard() {
   const { documents, error } = useCollection('projects');
   const [currentFilter, setCurrentFilter] = useState('all');
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>

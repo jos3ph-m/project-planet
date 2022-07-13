f0f2a46b49ecf96bec6a8d77de0aebb1c1700361
Joseph M <jmedina1985@hotmail.com>
2022-07-13 13:37:53 -0700

    Making a filter

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -33,6 +33,7 @@ export default function Dashboard() {
           case 'design':
           case 'sales':
           case 'marketing':
+            console.log(document.category, currentFilter);
             return document.category === currentFilter;
           default:
             return true;

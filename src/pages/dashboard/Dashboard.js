80c0e4e56a925f5c5f03100c285d9d7d9892141d
Joseph M <jmedina1985@hotmail.com>
2022-07-13 13:32:07 -0700

    Making a filter

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -16,28 +16,30 @@ export default function Dashboard() {
     setCurrentFilter(newFilter);
   };
 
-  const projects = documents ? documents.filter((document) => {
-    switch (currentFilter) {
-      case 'all':
-        return true;
-      case 'mine':
-        let assignedToMe = false;
-        document.assignedUsersList.forEach((u) => {
-          if (user.uid === u.id) {
-            assignedToMe = true;
-          }
-        });
-        return assignedToMe;
-      case 'development':
-      case 'design':
-      case 'sales':
-      case 'marketing':
-        console.log(document.category, currentFilter);
-        return document.category === currentFilter;
-      default:
-        return true;
-    }
-  });
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case 'all':
+            return true;
+          case 'mine':
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case 'development':
+          case 'design':
+          case 'sales':
+          case 'marketing':
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>

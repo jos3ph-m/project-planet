d6ab16cd77e173bda0de4d1a92e535854f8b06b9
Joseph M <jmedina1985@hotmail.com>
2022-07-24 14:37:01 -0700

    Filter adjustments

--- src/pages/dashboard/ProjectFilter.js
+++ src/pages/dashboard/ProjectFilter.js
@@ -1,4 +1,11 @@
-const filterList = ['all', 'mine', 'development', 'design', 'marketing'];
+const filterList = [
+  'all',
+  'mine',
+  'development',
+  'design',
+  'marketing',
+  'sales',
+];
 
 export default function ProjectFilter({ currentFilter, changeFilter }) {
   const handleClick = (newFilter) => {

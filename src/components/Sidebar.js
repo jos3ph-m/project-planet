0ae228f704b9822e27096cc9269e16ad9c8ecd25
Joseph M <jmedina1985@hotmail.com>
2022-08-05 10:10:53 -0700

    Adjustments to copy

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -27,7 +27,7 @@ export default function Sidebar() {
             <li>
               <NavLink to="/create">
                 <img src={AddIcon} alt="add project icon" />
-                <span>Next Project</span>
+                <span>New Project</span>
               </NavLink>
             </li>
           </ul>
